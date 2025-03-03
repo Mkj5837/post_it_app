b7b088784f6a8f63a8fb2442984ce66fefa4a5cc
Mkj5837 <malak.jabri333@gmail.com>
2025-03-03 12:04:49 +0400

    redux store

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>©2023.PostIT. All Rights Reserved.</div>
+      <div>Malak 16J21113 ©2023.PostIT. All Rights Reserved.</div>
     </footer>
   );
 };
